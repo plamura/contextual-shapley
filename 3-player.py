0d5e36034b260b7c00fbf6cc8cbecf0becef3792
plamura <30545564+plamura@users.noreply.github.com>
2019-06-27 18:45:40 +0200

    Update and rename direct.py to 3-player.py

--- 3-player.py
+++ 3-player.py
@@ -19,40 +19,36 @@ J = diag(a)
 D = TT.dot(J).dot(TT.T)
 G = H.dot(TT.T)
 
-M1 = matrix('0 1 0 0 0 0 0 0; 0 0 0 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 0 0 0 0 1 0 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 1; 0 0 0 0 0 0 0 0')
-M2 = matrix('0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 1 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 1; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0')
-M3 = matrix('0 0 0 1 0 0 0 0; 0 0 0 0 0 1 0 0; 0 0 0 0 0 0 1 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 1; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0')
-
 T1 = matrix('0 1 0 0 0 0 0 0; 1 0 0 0 0 0 0 0; 0 0 0 0 1 0 0 0; 0 0 0 0 0 1 0 0; 0 0 1 0 0 0 0 0; 0 0 0 1 0 0 0 0; 0 0 0 0 0 0 0 1; 0 0 0 0 0 0 1 0')
 T2 = matrix('0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0; 1 0 0 0 0 0 0 0; 0 0 0 0 0 0 1 0; 0 1 0 0 0 0 0 0; 0 0 0 0 0 0 0 1; 0 0 0 1 0 0 0 0; 0 0 0 0 0 1 0 0')
 T3 = matrix('0 0 0 1 0 0 0 0; 0 0 0 0 0 1 0 0; 0 0 0 0 0 0 1 0; 1 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 1; 0 1 0 0 0 0 0 0; 0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0')
 
-
-D1 = M1.dot(D).dot(M1.T)
-D2 = M2.dot(D).dot(M2.T)
-D3 = M3.dot(D).dot(M3.T)
+D1 = T1.dot(D).dot(T1.T)
+D2 = T2.dot(D).dot(T2.T)
+D3 = T3.dot(D).dot(T3.T)
 
 K1 = diag(array([1,0,1,1,0,0,1,0]))
 K2 = diag(array([1,1,0,1,0,1,0,0]))
 K3 = diag(array([1,1,1,0,1,0,0,0]))
 
+DD1 = K1.dot(D1 - D).dot(K1)
+DD2= K2.dot(D2 - D).dot(K2)
+DD3= K3.dot(D3- D).dot(K3)
+
+d1 = matrix(diagonal(DD1))
+d2 = matrix(diagonal(DD2))
+d3 = matrix(diagonal(DD3))
+
 q1 = sqrt((1/6)*matrix('2 0 1 1 0 0 2 0'))
 q2 = sqrt((1/6)*matrix('2 1 0 1 0 2 0 0'))
 q3 = sqrt((1/6)*matrix('2 1 1 0 2 0 0 0'))
 
-DD1 = D1 - K1.dot(D).dot(K1)
-d1 = matrix(diagonal(DD1))
 g1 = matrix(square(q1.dot(G)))
-print(d1.dot(g1.T))
-
-DD2= D2 - K2.dot(D).dot(K2)
-d2 = matrix(diagonal(DD2))
 g2 = matrix(square(q2.dot(G)))
-print(d2.dot(g2.T))
-
-DD3= D3- K3.dot(D).dot(K3)
-d3 = matrix(diagonal(DD3))
 g3 = matrix(square(q3.dot(G)))
+
+print(d1.dot(g1.T))
+print(d2.dot(g2.T))
 print(d3.dot(g3.T))
 
 
@@ -69,6 +65,6 @@ print(q2.dot(W2).dot(Q2.dot(V).dot(Q2) - V).dot(W2).dot(q2.T))
 print(q3.dot(W3).dot(Q3.dot(V).dot(Q3) - V).dot(W3).dot(q3.T))
 
 
-print(q1.dot(W1).dot(Q1).dot(V).dot(Q1).dot(W1).dot(q1.T) - q1.dot(W1).dot(V).dot(W1).dot(q1.T))
-print(q2.dot(W2).dot(Q2).dot(V).dot(Q2).dot(W2).dot(q2.T) - q2.dot(W2).dot(V).dot(W2).dot(q2.T))
-print(q3.dot(W3).dot(Q3).dot(V).dot(Q3).dot(W3).dot(q3.T) - q3.dot(W3).dot(V).dot(W3).dot(q3.T))
+#print(q1.dot(W1).dot(Q1).dot(V).dot(Q1).dot(W1).dot(q1.T) - q1.dot(W1).dot(V).dot(W1).dot(q1.T))
+#print(q2.dot(W2).dot(Q2).dot(V).dot(Q2).dot(W2).dot(q2.T) - q2.dot(W2).dot(V).dot(W2).dot(q2.T))
+#print(q3.dot(W3).dot(Q3).dot(V).dot(Q3).dot(W3).dot(q3.T) - q3.dot(W3).dot(V).dot(W3).dot(q3.T))
